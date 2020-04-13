c41f848c2583a195a8aadb6d9e98d6bb1e317596
Caroline Glass <carolineglass89@gmail.com>
2020-04-13 23:08:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,37 @@
 #top level of hash has two keys (:home => {} and :away => {})
+=begin {
+:home => {
+    :team_name => ""
+    :colors => ["", ""]
+    :players =>
+        [{:player_name => ""},
+        {:number => ""},
+        {:shoe => ""},
+        {:points => ""},
+        {:rebounds => ""},
+        {:assists => ""},
+        {:steals => ""},
+        {:blocks => ""},
+        {:slam_dunks => ""}
+        ]
+}
+:away => {
+    :team_name => ""
+    :colors => ["", ""]
+    :players => [
+        {:player_name => ""},
+        {:number => ""},
+        {:shoe => ""},
+        {:points => ""},
+        {:rebounds => ""},
+        {:assists => ""},
+        {:steals => ""},
+        {:blocks => ""},
+        {:slam_dunks => ""}
+        ]
+}
+} =end
+
 def game_hash
   
 end
