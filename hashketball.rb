c0e1adb6d14b3187281ca71b5aac273bc71e8b4e
Caroline Glass <carolineglass89@gmail.com>
2020-04-13 23:23:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,37 +1,5 @@
 #top level of hash has two keys (:home => {} and :away => {})
 =begin 
-{
-:home => {
-    :team_name => ""
-    :colors => ["", ""]
-    :players =>
-        [{:player_name => ""},
-        {:number => ""},
-        {:shoe => ""},
-        {:points => ""},
-        {:rebounds => ""},
-        {:assists => ""},
-        {:steals => ""},
-        {:blocks => ""},
-        {:slam_dunks => ""}
-        ]
-}
-:away => {
-    :team_name => ""
-    :colors => ["", ""]
-    :players => [
-        {:player_name => ""},
-        {:number => ""},
-        {:shoe => ""},
-        {:points => ""},
-        {:rebounds => ""},
-        {:assists => ""},
-        {:steals => ""},
-        {:blocks => ""},
-        {:slam_dunks => ""}
-        ]
-}
-} 
 
 =end
 
@@ -39,12 +7,21 @@ def game_hash
   {:home => {
     :team_name => "Brooklyn Nets",
     :colors => ["Black", "White"],
-    :players => {}
+    :players => [
+      {:player_name => "Alan Anderson", :number => "0", :shoe => "16", :points => "22", :rebounds => "12", :assists => "12", :steals => "3", :blocks => "1", :slam_dunks => "1"},
+      {:player_name => "", :number => "", :shoe => "", :points => "", :rebounds => "", :assists => "", :steals => "", :blocks => "", :slam_dunks => ""},
+      {:player_name => "", :number => "", :shoe => "", :points => "", :rebounds => "", :assists => "", :steals => "", :blocks => "", :slam_dunks => ""},
+      {:player_name => "", :number => "", :shoe => "", :points => "", :rebounds => "", :assists => "", :steals => "", :blocks => "", :slam_dunks => ""},
+      {:player_name => "", :number => "", :shoe => "", :points => "", :rebounds => "", :assists => "", :steals => "", :blocks => "", :slam_dunks => ""}]
   },
   :away => {
     :team_name => "Charlotte Hornets",
     :colors => ["Turquoise", "Purple"],
-    :players => {}
+    :players => [
+      {:player_name => "", :number => "", :shoe => "", :points => "", :rebounds => "", :assists => "", :steals => "", :blocks => "", :slam_dunks => ""},
+      {:player_name => "", :number => "", :shoe => "", :points => "", :rebounds => "", :assists => "", :steals => "", :blocks => "", :slam_dunks => ""},
+      {:player_name => "", :number => "", :shoe => "", :points => "", :rebounds => "", :assists => "", :steals => "", :blocks => "", :slam_dunks => ""},
+      {:player_name => "", :number => "", :shoe => "", :points => "", :rebounds => "", :assists => "", :steals => "", :blocks => "", :slam_dunks => ""},
+      {:player_name => "", :number => "", :shoe => "", :points => "", :rebounds => "", :assists => "", :steals => "", :blocks => "", :slam_dunks => ""}]
   }}
-  
 end
